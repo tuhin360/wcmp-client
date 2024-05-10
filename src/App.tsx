7feacda24b841210a4a71beb8d7f0208baccbb98
tuhin <tuhin153695@gmail.com>
2024-05-10 19:14:48 +0600

    initital project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Winter Cloth Management Platform</h1>
+      <button className="btn btn-info">Info</button>
+      
+    </>
+  );
+}
+
+export default App;
